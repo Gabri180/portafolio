bf59ffa698579486de5d7074444302bcdc0242d6
Gabri180 <ys7b4rgtjc@privaterelay.appleid.com>
2025-12-09 19:24:42 +0100

    Configurado GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://gabriel-martinez.dev',
-  output: 'static',
+  site: 'https://Gabri180.github.io',
+  base: '/portafolio',
 });
